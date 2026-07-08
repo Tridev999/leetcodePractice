08616e47f8b34374711f3777d916b2ec5f4f66e2
R.Tridev <tridevrudraraju999@gmail.com>
2026-07-08 12:59:43 +0530

    Time: 15 ms (6.24%), Space: 19.1 MB (5.04%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<vector<int>> arr;
-    set<vector<int>> result;
+    set<vector<int>> result; // used to remove duplicates
     void csum(vector<int>& candidates,int idx,vector<int> curr){
         if(idx==candidates.size()){
             int s=0;
@@ -13,7 +13,7 @@ public:
             }
                 result.insert(res);
                 if(sz!=result.size()){
-                    arr.push_back(res);
+                    arr.push_back(res); // updating to the ans
                 }
             return;
             }
@@ -23,7 +23,7 @@ public:
         curr.pop_back();
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        sort(nums.begin(),nums.end()); // it must be done when ever ksame ans generated in diff order
         vector<int> curr;
         int idx=0;
         csum(nums,idx,curr);
